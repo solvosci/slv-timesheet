c13855aec07852113d09abb68c585c341f8c9e85
ChristianSantamaria <christiancameselle@hotmail.com>
2021-06-11 14:49:10 +0200

    [ADD] hr_timesheet_report_not_valued

---
+++ hr_timesheet_report_not_valued/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Timesheet Report Not Valued",
+    "summary": """
+        Adds a report that hides the time field.
+    """,
+    "version": "12.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Human Resources",
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-timesheet",
+    "depends": [
+        "hr_timesheet",
+    ],
+    "data": [
+        "report/hr_timesheet_report.xml",
+    ],
+}
