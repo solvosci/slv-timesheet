74251649c2459a8dfea6c17b5f8ef8734cd4d00c
resua <32322359+adriresu@users.noreply.github.com>
2022-12-05 12:36:09 +0100

    [ADD] hr_timesheet_day_week

---
+++ hr_timesheet_day_week/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    "name": "Add day of week management to timesheets",
+    "description": "Add day of week management to timesheets",
+    "category": "Services/Timesheets",
+    "version": "15.0.1.0.0",
+    "depends": ["hr_timesheet"],
+    "data": [
+        "views/hr_timesheet_view.xml",
+    ],
+    "author": "Solvos Consultoría Informática",
+    "website": "https://github.com/solvosci/slv-timesheet",
+    "license": "LGPL-3",
+    "installable": True,
+}
---
+++ hr_timesheet_day_week/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This addons add 1 new field to search by weekdays and weekends.
