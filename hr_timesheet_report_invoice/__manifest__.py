fff8d9bf08f3fd50668e745de09839256a4ba015
David Alonso // Solvos <david.alonso@solvos.es>
2022-12-23 17:28:27 +0100

    [ADD] hr_timesheet_report_invoice

---
+++ hr_timesheet_report_invoice/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    "name": "Timesheet Report - invoice name on header",
+    "description": "Adds invoice name to header of timesheets report",
+    "category": "Services/Timesheets",
+    "version": "15.0.1.0.0",
+    "depends": ["sale_timesheet"],
+    "data": [
+        "report/account_analytic_line_inherit.xml"
+    ],
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-timesheet",
+    "license": "LGPL-3",
+    "installable": True,
+}
