d26206dd334a9754f651a9573eff14b3dde971eb
resua <32322359+adriresu@users.noreply.github.com>
2023-01-03 10:41:25 +0100

    [ADD] hr_timesheet_menu_filters: Addon to add filters to search view

---
+++ hr_timesheet_menu_filters/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    "name": "Add filters timesheet",
+    "description": "Add new filters to search view",
+    "category": "Services/Timesheets",
+    "version": "15.0.1.0.0",
+    "depends": ["hr_timesheet"],
+    "data": [
+        "views/hr_timesheet_view.xml",
+    ],
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-timesheet",
+    "license": "LGPL-3",
+    "installable": True,
+}
