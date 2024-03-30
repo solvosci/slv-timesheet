86163a48880a243e40f39441375f4112187fb670
David Alonso // Solvos <david.alonso@solvos.es>
2024-03-30 12:14:29 +0100

    [ADD] hr_timesheet_task_domain_nostage
    Adds tasks without stage to task limited domain for timesheets.
    
    OCA's ``hr_timesheet_task_domain`` actually limits task domain to tasks with
    stage defined (and open), with this addon a task with no stage can be also
    selected.

---
+++ hr_timesheet_task_domain_nostage/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Task Log: limit Task by Project - enable no-stage tasks",
+    "summary": """
+        Adds tasks without stage to task limited domain for timesheets
+    """,
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-timesheet",
+    "depends": ["hr_timesheet_task_domain"],
+}
---
+++ hr_timesheet_task_domain_nostage/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+Adds tasks without stage to task limited domain for timesheets.
+
+OCA's ``hr_timesheet_task_domain`` actually limits task domain to tasks with
+stage defined (and open), with this addon a task with no stage can be also
+selected.
