4e6ded06b706950fe0fa04c34d9ed1096293f332
Iria Alonso <iria.alonso@solvos.es>
2024-08-07 13:04:41 +0200

    [ADD] hr_timesheet_disable_duplicate

---
+++ hr_timesheet_disable_duplicate/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Timesheet Disable Duplicate",
+    "summary": """
+        Disable the duplicate function in the timesheets.
+    """,
+    "version": "15.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Human Resources",
+    "author": "Solvos",
+    "website": "https://github.com/solvosci/slv-timesheet",
+    "depends": ["hr_timesheet"],
+}
