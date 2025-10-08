b027b05a51f281a20af74d484f674739a74fa12c
David Alonso // Solvos <david.alonso@solvos.es>
2025-10-08 15:51:11 +0200

    [ADD] project_timesheet_holidays_task_enable
    When a task is used in leave types configuration, it cannot be used filling manual timesheets.
    
    With this addon, it's possible to re-enable a task affected by this limitation.

---
+++ project_timesheet_holidays_task_enable/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2025 Solvos Consultoría Informática (<https://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    "name": "Project Timesheet Holidays - re-enable task",
+    "description": "Re-enables task used in leave types",
+    "category": "Human Resources",
+    "version": "17.0.1.0.0",
+    "depends": ["project_timesheet_holidays"],
+    "data": ["views/project_task_views.xml"],
+    "author": "Solvos Consultoría Informática",
+    "website": "https://github.com/solvosci/slv-timesheet",
+    "license": "LGPL-3",
+    "installable": True,
+}
