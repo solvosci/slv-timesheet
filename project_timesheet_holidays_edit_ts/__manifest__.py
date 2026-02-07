be8d231fede4bf34e808568f74fe446c4d0159ed
David Alonso // Solvos <david.alonso@solvos.es>
2026-02-07 13:48:52 +0100

    [ADD] project_timesheet_holidays_edit_ts
    By default, every timesheet generated from a leave becomes read-only, so nobody can directly edit it and e.g. change its hour count. This shouldn't occur in Odoo previous versions.
    
    This could be annoying, because for certain leaves (like medical leaves), final spent time should be different than initial leave filled. This forces to reject, modify and then approve again leave. Exact time could not be filled sometimes anyway, due to leave form hour selection.
    
    This addon re-enables timesheet edition when they're generated from leaves (initially name and quant fields), letting us make it editable depending on leave type.

---
+++ project_timesheet_holidays_edit_ts/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2026 Solvos Consultoría Informática (<https://www.solvos.es>)
+# License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    "name": "Project Timesheet Holidays - re-enable generated timesheeets edition",
+    "description": "Re-enables timesheet edition when they're generated from leaves",
+    "category": "Human Resources",
+    "version": "17.0.1.0.0",
+    "depends": ["project_timesheet_holidays"],
+    "data": ["views/hr_leave_type_views.xml"],
+    "author": "Solvos Consultoría Informática",
+    "website": "https://github.com/solvosci/slv-timesheet",
+    "license": "LGPL-3",
+    "installable": True,
+}
